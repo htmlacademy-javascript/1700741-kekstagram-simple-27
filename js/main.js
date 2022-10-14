22397157e7f172abb44e85165aa65cc28dbdaada
alenachesnokova <chesnokovaeg@mail.ru>
2022-10-14 17:58:15 +0500

    Добавила функции

--- js/main.js
+++ js/main.js
@@ -5,9 +5,28 @@ function getRandom (min, max) {
 
 getRandom(4,8);
 
+function getRandomVariant (min, max) {
+  if (min < 0 || max < 0) {return NaN;}
+
+  if (min > max) {let a = min; min = max; max = a;}
+
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+getRandomVariant(4,8);
+
+
 function checkLenghtString (checkString, lenghtString) {
+  if (!checkString || !lenghtString) {return NaN;}
   return (checkString < lenghtString);
 }
 
 checkLenghtString('hello', 5);
 
+function getCoordinate (min, max, numberDecimalePlaces) {
+  if (min > max || min < 0 || max < 0) {return NaN;}
+  let random = Math.random() * (max - min) + min;
+  return +random.toFixed(numberDecimalePlaces);
+}
+
+getCoordinate(1, 6, 7);
