ac57f2a8fa15778ddc8a5ba634a45b4c0ab13981
Keks <keks@htmlacademy.ru>
2022-10-19 07:51:47 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -3,11 +3,30 @@ function getRandom (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-getRandom(4,8);
+getRandom(5,8);
+
+function getRandomVariant (min, max) {
+  if (min < 0 || max < 0) {return NaN;}
+
+  if (min > max) {const a = min; min = max; max = a;}
+
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+getRandomVariant(5,8);
+
 
 function checkLenghtString (checkString, lenghtString) {
+  if (checkString || lenghtString) {return NaN;}
   return (checkString < lenghtString);
 }
 
 checkLenghtString('hello', 5);
 
+function getCoordinate (min, max, numberDecimalePlaces) {
+  if (min > max || min < 0 || max < 0) {return NaN;}
+  const random = Math.random() * (max - min) + min;
+  return +random.toFixed(numberDecimalePlaces);
+}
+
+getCoordinate(1, 6, 7);
